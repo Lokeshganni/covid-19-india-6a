17eb469a5b59dae93d6a60a51c971657f93a72d9
Lokeshganni <lokeshganni@gmail.com>
2023-08-02 11:11:32 +0000

    coding practice 6a

---
+++ app.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const app = express();
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "covid19India.db");
+
+let db;
+
+const initialiseDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server running at PORT 3000");
+    });
+  } catch (e) {
+    console.log(`DB ERROR: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initialiseDbAndServer();
+
+const snakeToPascalCase = (obj) => {
+  return {
+    stateId: obj.state_id,
+    stateName: obj.state_name,
+    population: obj.population,
+  };
+};
+
+// API 1 list of all states from state table
+
+app.get("/states/", async (req, res) => {
+  const API1Query = `
+    SELECT * FROM state;`;
+  const API1Res = await db.all(API1Query);
+  res.send(API1Res.map((obj) => snakeToPascalCase(obj)));
+});
+
+//API 2 state based on stateId
+
+app.get("/states/:stateId/", async (req, res) => {
+  const { stateId } = req.params;
+  const API2Query = `
+  SELECT * FROM state WHERE state_id=${stateId};`;
+  const API2Res = await db.get(API2Query);
+  res.send(snakeToPascalCase(API2Res));
+});
+
+//API 3 posting to district table
+
+app.post("/districts/", async (req, res) => {
+  const postObjDetails = req.body;
+  const {
+    districtName,
+    stateId,
+    cases,
+    cured,
+    active,
+    deaths,
+  } = postObjDetails;
+  API3Query = `
+  INSERT INTO district(district_name,state_id,cases,cured,active,deaths)
+  VALUES('${districtName}',${stateId},${cases},${cured},${active},${deaths});`;
+  const API3Res = await db.run(API3Query);
+  const postApiDistrictId = API3Res.lastID;
+  res.send("District Successfully Added");
+});
+
+const snakeCaseToPascalCaseForDistrict = (obj) => {
+  return {
+    districtId: obj.district_id,
+    districtName: obj.district_name,
+    stateId: obj.state_id,
+    cases: obj.cases,
+    cured: obj.cured,
+    active: obj.active,
+    deaths: obj.deaths,
+  };
+};
+
+//API 4 get district from district table
+
+app.get("/districts/:districtId/", async (req, res) => {
+  const { districtId } = req.params;
+  const API4Query = `
+    SELECT * FROM district WHERE district_id=${districtId};`;
+  const API4Res = await db.get(API4Query);
+  res.send(snakeCaseToPascalCaseForDistrict(API4Res));
+});
+
+//API 5 delete district from district table
+
+app.delete("/districts/:districtId/", async (req, res) => {
+  const { districtId } = req.params;
+  const API5query = `DELETE FROM district WHERE district_id=${districtId};`;
+  await db.run(API5query);
+  res.send("District Removed");
+});
+
+//API 6 update district table
+
+app.put("districts/:districtId/", async (req, res) => {
+  const { districtId } = req.params;
+  const putObjDetails = req.body;
+  const { districtName, stateId, cases, cured, active, deaths } = putObjDetails;
+  const API6Query = `
+  UPDATE district
+  SET 
+  district_name='${districtName}',
+  state_id=${stateId},
+  cases=${cases},
+  cured=${cured},
+  active=${active},
+  deaths=${deaths}
+  WHERE district_id=${districtId};`;
+  await db.run(API6Query);
+  res.send("District Details Updated");
+});
+
+//API 7
+
+module.exports = app;
